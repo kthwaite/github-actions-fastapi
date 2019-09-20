d5c306360c8ebcad52fd315d5dc8e6a7cf72ecf6
cthwaite <thwaite@mac.com>
2019-09-20 14:01:27 +0100

    style(black): Fix style in app.py

--- app.py
+++ app.py
@@ -7,22 +7,22 @@ from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 
-api = FastAPI('github actions demo')
+api = FastAPI("github actions demo")
 
 
 class GreetingStyle(str, Enum):
-    Affable = 'affable'
-    Unpleasant = 'unpleasant'
-    Aggressive = 'aggressive'
-    Diffident = 'diffident'
+    Affable = "affable"
+    Unpleasant = "unpleasant"
+    Aggressive = "aggressive"
+    Diffident = "diffident"
 
 
 _STYLE_NAMES = tuple(entry.value for entry in GreetingStyle)
 _GREETING_FOR_STYLE = {
-    GreetingStyle.Affable: 'Well hey there, {name}!',
+    GreetingStyle.Affable: "Well hey there, {name}!",
     GreetingStyle.Unpleasant: "Oh. It's you again, {name}.",
-    GreetingStyle.Diffident: 'Oh! Hi, uh... hi {name}...',
-    GreetingStyle.Aggressive: 'Well? What do you want, {name}?',
+    GreetingStyle.Diffident: "Oh! Hi, uh... hi {name}...",
+    GreetingStyle.Aggressive: "Well? What do you want, {name}?",
 }
 
 
@@ -32,16 +32,16 @@ class GreetingResponseModel(BaseModel):
     style: GreetingStyle
 
 
-@api.get('/api/v1/greet', response_model=GreetingResponseModel)
-def get_greeting(name: str = Query(..., description='Name to greet')):
+@api.get("/api/v1/greet", response_model=GreetingResponseModel)
+def get_greeting(name: str = Query(..., description="Name to greet")):
     """Greet a user by name, with a randomly-chosen greeting style.
     """
-    if name.lower() == 'joe':
+    if name.lower() == "joe":
         style = GreetingStyle.Aggressive
     else:
         style = random.choice(list(GreetingStyle))
     return {
-        'greeting': _GREETING_FOR_STYLE[style].format(name=name),
-        'style': style,
-        'name': name,
+        "greeting": _GREETING_FOR_STYLE[style].format(name=name),
+        "style": style,
+        "name": name,
     }
